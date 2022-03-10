b55cd9071d36deb3cd8d8d67dc0aba95472f6ed9
Bryan J. Brown <bjbrown@fsu.edu>
2022-03-10 13:29:28 -0600

    Adds logging to Vagrant deployment

--- boot.sh
+++ boot.sh
@@ -4,5 +4,5 @@ echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> /etc/environment
 echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> /etc/environment
 echo "AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION" >> /etc/environment
 
-git clone --quiet https://$GITHUB_TOKEN:x-oauth-basic@github.com/fsulib/cf-libapp-drupal9 /cf-libapp-drupal9
-/cf-libapp-drupal9/build/vagrant.sh
+git clone --quiet https://$GITHUB_TOKEN:x-oauth-basic@github.com/fsulib/cf-libapp-drupal9 /cf-libapp-drupal9 >> /root/build.log 2>&1
+/cf-libapp-drupal9/build/vagrant.sh >> /root/build.log 2>&1
