0f09daf626e91fbca78ecdb19d0dc5f0470de2e4
Bryan J. Brown <bjbrown@fsu.edu>
2022-03-15 14:50:14 -0500

    Updates boot.sh to account for new repos directory

--- boot.sh
+++ boot.sh
@@ -4,5 +4,7 @@ echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> /etc/environment
 echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> /etc/environment
 echo "AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION" >> /etc/environment
 
-git clone --quiet https://$GITHUB_TOKEN:x-oauth-basic@github.com/fsulib/drupal9_deployment /drupal9_deployment >> /root/build.log 2>&1
-/drupal9_deployment/build/vagrant.sh >> /root/build.log 2>&1
+cd /var
+mkdir repos
+git clone --quiet https://$GITHUB_TOKEN:x-oauth-basic@github.com/fsulib/drupal9_deployment /var/repos/drupal9_deployment >> /root/build.log 2>&1
+/var/repos/drupal9_deployment/build/vagrant.sh >> /root/build.log 2>&1
